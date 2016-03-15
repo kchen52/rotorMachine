ab1472415034096d35dfaf8b69842fd69155e18f
Kevin Chen <sca123@sfu.ca>
2016-03-14 23:51:39 -0700

    Can set up rotors based on predefined init values

--- RotorMachine.java
+++ RotorMachine.java
@@ -2,17 +2,30 @@ import java.util.ArrayList;
 
 class RotorMachine {
     // Need at least 5 security schemes
-    String securitySchemes[] = {"7-11-13"};
+    private static String securitySchemes[] = {"7-11-13"};
 
     // Need at least 2 rotors
-    ArrayList<Rotor> rotors = new ArrayList<Rotor>();
+    private static ArrayList<Rotor> rotors = new ArrayList<Rotor>();
 
     public static void main(String args[]) {
         int securitySchemeChosen = 0;
         String securityScheme = securitySchemes[securitySchemeChosen];
-        //int numberOfRotors = StringUtils.countMatches(securityScheme, "-")+1;
+        int numberOfRotors = numberOfTimesCharAppeared('-', securityScheme)+1;
 
-        //System.out.println("Number of rotors: " + numberOfRotors);
+        for (int i = 0; i < numberOfRotors; i++) {
+            int rotorInitValue = Integer.parseInt(securityScheme.split("-")[i]);
+            System.out.println("Rotor Init value: " + rotorInitValue);
+            rotors.add(new Rotor(rotorInitValue));
+        }
+    }
 
+    private static int numberOfTimesCharAppeared(char lookFor, String temp) {
+        int counter = 0;
+        for (int i = 0; i < temp.length(); i++) {
+            if (temp.charAt(i) == lookFor) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
