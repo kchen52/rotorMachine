abe68053558642043ea05a7331206b41a546f2e8
lf52 <k.chen52@gmail.com>
2016-03-16 16:02:39 -0700

    Added server sided stuff... need to do testing now.

--- MyMessagingApp.java
+++ MyMessagingApp.java
@@ -19,7 +19,9 @@ class App {
 		}
 
 		if (clientMode) {
+			// ---------------------------------------------------------------
 			// Do client stuff here...
+			// ---------------------------------------------------------------
 			System.out.print("Enter the server's address: ");
 			String serverAddress = System.console().readLine();
 
@@ -57,9 +59,47 @@ class App {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			// ---------------------------------------------------------------
 			// END OF CLIENT STUFF
+			// ---------------------------------------------------------------
 		} else if (serverMode) {
+			// ---------------------------------------------------------------
 			// Do server stuff here
+			// ---------------------------------------------------------------
+			ServerSocket serverSocket;
+			Socket clientSocket;
+
+			System.out.print("Enter the port number: ");
+			// TODO: error handling for bad input here
+			int portNumber = Integer.parseInt(System.console().readLine());
+
+			try {
+				serverSocket = new ServerSocket(portNumber);
+				clientSocket = serverSocket.accept();
+				while (true) {
+					BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+					DataOutputStream toClient = new DataOutputStream(clientSocket.getOutputStream());
+
+					String encryptedTextFromClient = fromClient.readLine();
+					String decryptedTextFromClient = RotorMachine.decryptMessage(encryptedTextFromClient);
+					
+					// Take user input, encrypt it, and send it to client
+					System.out.print("Enter text: ");
+					String sentence = System.console().readLine();
+
+					// TODO: SET SECURITY SCHEME DYNAMICALLY
+					String encryptedMessage = RotorMachine.encryptMessage(sentence);
+					toClient.writeBytes(encryptedMessage + "\n");
+				}
+			} catch (SocketException e) {
+				/*clientSocket.close();
+				  serverSocket.close();*/
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				/*clientSocket.close();
+				  serverSocket.close();*/
+			}
 		} 
 		return;
 	}
